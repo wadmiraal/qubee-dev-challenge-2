1be7093f85e1c9ae17f92aa7b83925ea23db2262
Zipeng WU <zipeng.wu@sonarsource.com>
2023-09-21 13:41:24 +0200

    Day 8 part 2

---
+++ src/main/java/fr/sma/aoc/adventofcode2015/solution/day08/Day08Ex2.java
@@ -0,0 +1,48 @@
+package fr.sma.aoc.adventofcode2015.solution.day08;
+
+import fr.sma.aoc.adventofcode2015.ExSolution;
+import fr.sma.aoc.adventofcode2015.ResolveApplication;
+import fr.sma.aoc.adventofcode2015.solution.day03.Day03Ex1;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class Day08Ex2 implements ExSolution {
+
+
+    Logger logger = LoggerFactory.getLogger(Day03Ex1.class);
+
+    public static void main(String[] args) {
+        ResolveApplication.main(new String[]{"08", "2"});
+    }
+
+    @Override
+    public String run(String input) {
+        var lines = input.split("\n");
+        long result = 0;
+        for (var line : lines) {
+            result += count(line.trim());
+
+        }
+        logger.info("" + result);
+        return "" + result;
+    }
+
+    private long count(String line) {
+        int ret = 0;
+        int len1 = line.length();
+        String line2 = line.replace("\\\\", "");
+        int len2 = line2.length();
+        ret += (len1-len2);
+        int len3 = line2.replace("\\\"", "").length();
+        ret += (len2-len3);
+        int len4 = line2.replace("\\x", "").length();
+        ret += (len2-len4) /2;
+        ret += 4;
+        return ret;
+
+    }
+
+}
