4742f1d7673e8078907a77f87054bcc885923eb4
Steve Marion <steve.marion@sonarsource.com>
2023-09-21 09:50:37 +0200

    add first day skeletton

---
+++ src/main/java/fr/sma/aoc/adventofcode2022/solution/day01/Day01Ex1.java
@@ -0,0 +1,17 @@
+package fr.sma.aoc.adventofcode2022.solution.day01;
+
+import fr.sma.aoc.adventofcode2022.ExSolution;
+import fr.sma.aoc.adventofcode2022.ResolveApplication;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Day01Ex1 implements ExSolution {
+
+  public static void main(String[] args) {
+    ResolveApplication.main(new String[]{"01", "1"});
+  }
+  @Override
+  public String run(String input) throws Exception {
+    throw new IllegalStateException();
+  }
+}
