13237be14ed8f629edc32aca8d8691da9967412c
Eric Giffon <eric.giffon@sonarsource.com>
2023-09-22 09:57:29 +0200

    day 14 - part1

--- src/main/java/fr/sma/aoc/adventofcode2015/solution/day14/Day14Ex1.java
+++ src/main/java/fr/sma/aoc/adventofcode2015/solution/day14/Day14Ex1.java
@@ -5,15 +5,19 @@ import fr.sma.aoc.adventofcode2015.ResolveApplication;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Day14Ex1 implements ExSolution {
+  public static final int RACE_DURATION = 2503;
 
   public static void main(String[] args) {
-    ResolveApplication.main(new String[]{"14", "1"});
+    ResolveApplication.main(new String[] {"14", "1"});
   }
 
   @Override
@@ -30,7 +34,30 @@ public class Day14Ex1 implements ExSolution {
       }
     }
 
-    return "";
+    Map<Reindeer, Integer> reindeerToDistance = reindeers.stream()
+      .collect(Collectors.toMap(Function.identity(), r -> computeDistance(RACE_DURATION, r)));
+
+    System.out.println(reindeerToDistance);
+
+    return String.valueOf(reindeerToDistance.values().stream()
+      .mapToInt(i -> i)
+      .max()
+      .orElseThrow());
+  }
+
+  private Integer computeDistance(int raceDuration, Reindeer reindeer) {
+
+    int cycleTime = reindeer.restTime() + reindeer.flyTime();
+    int cycleDistance = reindeer.speed() * reindeer.flyTime();
+
+    int nbCycles = raceDuration / cycleTime;
+
+    int totalDistance = nbCycles * cycleDistance;
+
+    int remainingSeconds = RACE_DURATION % cycleTime;
+
+    int remainingFlyTime = Math.min(remainingSeconds, reindeer.flyTime());
+    return totalDistance + remainingFlyTime * reindeer.speed();
   }
 
   private record Reindeer(String name, int speed, int flyTime, int restTime) {
