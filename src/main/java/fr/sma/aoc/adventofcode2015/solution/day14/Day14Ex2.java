6f5624fb82a1612a5bff071f30804cfc1afe46bd
Aurelien Poscia <aurelien.poscia@sonarsource.com>
2023-09-22 10:52:31 +0200

    Day 14 - part 2

--- src/main/java/fr/sma/aoc/adventofcode2015/solution/day14/Day14Ex2.java
+++ src/main/java/fr/sma/aoc/adventofcode2015/solution/day14/Day14Ex2.java
@@ -3,9 +3,11 @@ package fr.sma.aoc.adventofcode2015.solution.day14;
 import fr.sma.aoc.adventofcode2015.ExSolution;
 import fr.sma.aoc.adventofcode2015.ResolveApplication;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,11 +15,11 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Day14Ex1 implements ExSolution {
+public class Day14Ex2 implements ExSolution {
   public static final int RACE_DURATION = 2503;
 
   public static void main(String[] args) {
-    ResolveApplication.main(new String[] {"14", "1"});
+    ResolveApplication.main(new String[] {"14", "2"});
   }
 
   @Override
@@ -34,8 +36,48 @@ public class Day14Ex1 implements ExSolution {
       }
     }
 
+    Map<Reindeer, Integer> reindeerToPosition = new HashMap<>();
+    Map<Reindeer, Integer> reindeerToScore = new HashMap<>();
 
-    Map<Reindeer, Integer> reindeerToDistance = reindeers.stream()
+    for (int currentTime = 0; currentTime < RACE_DURATION; currentTime++) {
+      for (Reindeer reindeer : reindeers) {
+        reindeerToPosition.putIfAbsent(reindeer, 0);
+        reindeerToScore.putIfAbsent(reindeer, 0);
+
+        int cycleTime = reindeer.restTime() + reindeer.flyTime();
+
+        if (currentTime % cycleTime < reindeer.flyTime()) {
+          reindeerToPosition.compute(reindeer, (reinder, position) -> position + reindeer.speed());
+        }
+      }
+
+      Set<Reindeer> iterationWinners = new HashSet<>();
+      int bestPosition = 0;
+      for (Map.Entry<Reindeer, Integer> reinderToPos : reindeerToPosition.entrySet()) {
+        Reindeer reindeer = reinderToPos.getKey();
+        Integer position = reinderToPos.getValue();
+        if (position > bestPosition) {
+          iterationWinners.clear();
+          bestPosition = position;
+          iterationWinners.add(reindeer);
+        }
+        if (position == bestPosition) {
+          iterationWinners.add(reindeer);
+        }
+      }
+      iterationWinners.forEach(reindeer ->
+        reindeerToScore.compute(reindeer, (r, previousScore) -> previousScore + 1));
+
+    }
+
+    return String.valueOf(
+      reindeerToScore.values().stream()
+        .mapToInt(i -> i)
+        .max().orElseThrow()
+    );
+
+
+    /*Map<Reindeer, Integer> reindeerToDistance = reindeers.stream()
       .collect(Collectors.toMap(Function.identity(), r -> computeDistance(RACE_DURATION, r)));
 
     System.out.println(reindeerToDistance);
@@ -43,7 +85,7 @@ public class Day14Ex1 implements ExSolution {
     return String.valueOf(reindeerToDistance.values().stream()
       .mapToInt(i -> i)
       .max()
-      .orElseThrow());
+      .orElseThrow());*/
   }
 
   private Integer computeDistance(int raceDuration, Reindeer reindeer) {
